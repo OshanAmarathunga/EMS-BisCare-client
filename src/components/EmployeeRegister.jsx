1b89c0a164ff3cf676ba4929ebdb84d6bce465cb
oshanamarathunga <oshanamarathunga1@gmail.com>
2025-09-01 00:37:16 +0530

    commit-31-8

--- src/components/EmployeeRegister.jsx
+++ src/components/EmployeeRegister.jsx
@@ -1,41 +1,332 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@mui/material/Button';
+import axios from "axios";
+import { Toaster, toast } from 'sonner'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CreateIcon from '@mui/icons-material/Create';
+import { Dialog, DialogTitle, DialogContent, DialogActions,TextField ,FormControl,InputLabel,Select, MenuItem} from "@mui/material";
 
 const EmployeeRegister = () => {
+
+    const [firstName,setFirstName]=useState(null);
+    const [lastName,setLastName]=useState(null);
+    const [empNo,setEmpNo]=useState(null);
+    const [contactNo,setContactNo]=useState(null);
+    const [gender,setGender]=useState(null);
+    const [salaryCategoryId,setSalaryCategoryId]=useState(0);
+    const [active,setActive]=useState(false);
+    const [mongoId,setMongoID ]=useState("");
+    const url=import.meta.env.VITE_SERVER_URL;
+    const [open, setOpen] = React.useState(false);
+    const [employeeList,setEmployeeList]=useState([]);
+    const [categoryList,setCategoryList]=useState([]);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [categoryId,setCategoryId]=useState(null);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+        clear();
+    };
+
+    const handleClickOpen = (emp) => {
+        setFirstName(emp.firstName);
+        setLastName(emp.lastName);
+        setEmpNo(emp.empNo);
+        setContactNo(emp.contactNo);
+        setGender(emp.gender);
+        setSalaryCategoryId(emp.salaryCategoryId);
+        setActive(emp.isActive);
+        setOpenDialog(true);
+        setMongoID(emp._id);
+    };
+
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        handleOpen();
+        const data={
+            "firstName":firstName,
+            "lastName": lastName,
+            "empNo": empNo,
+            "contactNo": contactNo,
+            "gender":gender,
+            "salaryCategoryId":salaryCategoryId
+
+        };
+        axios.post(url+"/v1/employee/saveEmployee",data)
+            .then((res)=>{
+                clear();
+                handleClose();
+                getAllEmployees();
+                toast.success('Employee saved successfully');
+            }).catch((e)=>{
+            console.log(e)
+            handleClose()
+            toast.error('Not saved : '+e.response.data.saveError);
+        });
+    }
+
+    const clear=()=>{
+        setFirstName("");
+        setLastName("");
+        setEmpNo("");
+        setContactNo("");
+        setGender("")
+        setSalaryCategoryId(0)
+    }
+    const getAllCategories=()=>{
+        axios.get(url+"/v1/salary-category/get-all-categories")
+            .then((res)=>{
+                setCategoryList(res.data.categoryList);
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+    }
+
+    const getAllEmployees=()=>{
+        axios.get(url+"/v1/employee/get-all-employee")
+            .then((res)=>{
+                setEmployeeList(res.data.employeeList);
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+    }
+
+    const handleUpdate=()=>{
+        //handleOpen();
+        const data={
+            "firstName":firstName,
+            "lastName": lastName,
+            "empNo": empNo,
+            "contactNo": contactNo,
+            "gender":gender,
+            "salaryCategoryId":salaryCategoryId,
+            "isActive":active
+        };
+
+        axios.put(url+`/v1/employee/update-employee/${mongoId}`,data)
+            .then((res)=>{
+                handleCloseDialog();
+                getAllEmployees();
+                handleClose();
+                clear();
+                toast.success('Employee updated!');
+            })
+            .catch((e)=>{
+                console.log(e)
+                handleClose();
+                toast.error('Not updated : '+e.response.data.message);
+            })
+    }
+
+    useEffect(()=>{
+        getAllEmployees();
+        getAllCategories();
+    },[])
     return (
         <div>
-            <h1 className='text-2xl font-bold'>Registration</h1>
+
+            <Toaster richColors position="top-center" />
+            <Backdrop
+                sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                open={open}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <h1 className='text-2xl font-bold'>Employee Registration</h1>
             <br/>
-            <form>
+            <form onSubmit={handleSubmit}>
+
                 <div className='my-2'>
                     <label>First Name</label>
-                    <input className='ml-[120px] bg-amber-50' type='text' required/>
+                    <input value={firstName} onChange={(e)=>setFirstName(e.target.value)} required className='ml-[70px] bg-amber-50' type='text'/>
                 </div>
                 <div className='my-2'>
                     <label>Last Name</label>
-                    <input className='ml-[120px] bg-amber-50' type='text'/>
+                    <input value={lastName} onChange={(e)=>setLastName(e.target.value)} className='ml-[70px] bg-amber-50' type='text'/>
                 </div>
                 <div className='my-2'>
-                    <label>Employee Number</label>
-                    <input className='ml-[65px] bg-amber-50' type='text' required/>
+                    <label>EMP No</label>
+                    <input value={empNo} onChange={(e)=>setEmpNo(e.target.value)} required className='ml-[90px] bg-amber-50' type='text'/>
                 </div>
                 <div className='my-2'>
-                    <label>Contact Number</label>
-                    <input className='ml-[80px] bg-amber-50' type='text'/>
+                    <label>Contact No</label>
+                    <input value={contactNo} onChange={(e)=>setContactNo(e.target.value)}  className='ml-[70px] bg-amber-50' type='text'/>
                 </div>
-
                 <div className='my-2'>
                     <label>Gender</label>
-                    <input className='ml-[145px] bg-amber-50' type='text'/>
+                    <select
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                        className='ml-[95px] bg-amber-50 border rounded px-2 py-1'
+                        required
+                    >
+                        <option value="">Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
                 </div>
                 <div className='my-2'>
                     <label>Salary Category Id</label>
-                    <input type='text'/>
+                    <select
+                        value={salaryCategoryId}
+                        onChange={(e) => setSalaryCategoryId(e.target.value)}
+                        className='ml-[15px] bg-amber-50 border rounded px-2 py-1'
+                        required
+                    >
+                        <option value="">Select Category</option>
+                        {
+                            categoryList.map((category)=>(
+                            <option key={category.categoryId} value={category.categoryId}>{category.categoryName}  |  Rs.{category.perHourRate}</option>
+                            ))
+                        }
+
+                    </select>
                 </div>
                 <div className='flex items-center justify-between'>
                     <Button variant="contained" color="success" type="submit">
-                    Save
+                        Save
                     </Button>
+                </div>
+
+                <hr className='my-5'/>
+                <div  className="w-full overflow-hidden">
+                    <TableContainer  sx={{
+                        maxWidth:{
+                            xs: 500, md: '100%'
+                        }
+                    }}>
+                        <Table  size="small" aria-label="a dense table" sx={{ minWidth: 900 }}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Employee NO</TableCell>
+                                    <TableCell align="center">First Name</TableCell>
+                                    <TableCell align="center">Last Name</TableCell>
+                                    <TableCell align="center">Contact No</TableCell>
+                                    <TableCell align="center">Gender</TableCell>
+                                    <TableCell align="center">Salary Category</TableCell>
+                                    <TableCell align="center">Status</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {employeeList.map((emp) => (
+                                    <TableRow
+                                        key={emp._id}
+                                    >
+                                        <TableCell align="center">{emp.firstName}</TableCell>
+                                        <TableCell align="center">{emp.lastName}</TableCell>
+                                        <TableCell align="center">{emp.empNo}</TableCell>
+                                        <TableCell align="center">{emp.contactNo}</TableCell>
+                                        <TableCell align="center">{emp.gender}</TableCell>
+                                        <TableCell align="center">{emp.salaryCategory[0]?.categoryName} | Rs.{emp.salaryCategory[0]?.perHourRate} </TableCell>
+                                        <TableCell align="center">{emp.isActive==true?"Active":"Inactive"}</TableCell>
+                                        <TableCell align="center"><CreateIcon onClick={()=>handleClickOpen(emp)}  style={{ cursor: "pointer" }}/></TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+
+                <div >
+                    <Dialog open={openDialog} onClose={handleClose}>
+                        <DialogTitle>Update Employee</DialogTitle>
+                        <DialogContent>
+                            <TextField
+                                margin="dense"
+                                label="First name"
+                                name="firstName"
+                                fullWidth
+                                value={firstName}
+                                onChange={(e)=>setFirstName(e.target.value)}
+                            />
+                            <TextField
+                                margin="dense"
+                                label="Last name"
+                                name="lastName"
+                                fullWidth
+                                value={lastName}
+                                onChange={(e)=>setLastName(e.target.value)}
+                            />
+                            <TextField
+                                margin="dense"
+                                label="Emp No"
+                                name="empNo"
+                                fullWidth
+                                value={empNo}
+                                onChange={(e)=>setEmpNo(e.target.value)}
+                            />
+                            <TextField
+                                margin="dense"
+                                label="Contact No"
+                                name="contactNo"
+                                fullWidth
+                                value={contactNo}
+                                onChange={(e)=>setContactNo(e.target.value)}
+                            />
+                            <FormControl fullWidth margin="dense">
+                                <InputLabel id="gender-label">Gender</InputLabel>
+                                <Select
+                                    labelId="gender-label"
+                                    value={gender}
+                                    onChange={(e) => setGender(e.target.value)}
+                                >
+                                    <MenuItem value="Male">Male</MenuItem>
+                                    <MenuItem value="Female">Female</MenuItem>
+                                </Select>
+                            </FormControl>
+
+                            <FormControl fullWidth margin="dense">
+                                <InputLabel id="Salary category">Salary category</InputLabel>
+                                <Select
+                                    labelId="Salary category"
+                                    value={salaryCategoryId}
+                                    onChange={(e) => setSalaryCategoryId(e.target.value)}
+                                >
+                                    {categoryList.map((category)=>(
+                                    <MenuItem key={category._id} value={category.categoryId}>{category.categoryName}    ==>  Rs.{category.perHourRate}</MenuItem>
+                                    ))}
+
+                                </Select>
+                            </FormControl>
+
+                            <FormControl fullWidth margin="dense">
+                                <InputLabel id="status-label">Status</InputLabel>
+                                <Select
+                                    labelId="status-label"
+                                    value={active}
+                                    onChange={(e) => setActive(e.target.value)}
+                                >
+                                    <MenuItem value='true'>Active</MenuItem>
+                                    <MenuItem value='false'>Inactive</MenuItem>
+                                </Select>
+                            </FormControl>
+
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleCloseDialog}>Cancel</Button>
+                            <Button variant="contained" onClick={handleUpdate}>
+                                Update
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
 
             </form>
--- src/components/SalaryCategory.jsx
+++ src/components/SalaryCategory.jsx
@@ -1,6 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@mui/material/Button';
 import axios from "axios";
+import { Toaster, toast } from 'sonner'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CreateIcon from '@mui/icons-material/Create';
+import { Dialog, DialogTitle, DialogContent, DialogActions,TextField } from "@mui/material";
 
 const SalaryCategory = () => {
 
@@ -9,9 +21,35 @@ const SalaryCategory = () => {
     const [rate,setRate]=useState(0);
     const [note,setNote]=useState(null);
     const url=import.meta.env.VITE_SERVER_URL;
+    const [open, setOpen] = React.useState(false);
+    const [categoryList,setCategoryList]=useState([]);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [categoryId,setCategoryId]=useState(null);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+        clear();
+    };
+
+    const handleClickOpen = (category) => {
+        setName(category.categoryName);
+        setRate(category.perHourRate);
+        setNote(category.note);
+        setCategoryId(category._id);
+        setOpenDialog(true);
+    };
+
 
     const handleSubmit=(e)=>{
         e.preventDefault();
+        handleOpen();
         const data={
             "categoryName":name,
             "perHourRate": rate,
@@ -19,33 +57,155 @@ const SalaryCategory = () => {
         };
         axios.post(url+"/v1/salary-category/save-salary-category",data)
             .then((res)=>{
-            console.log(res.data);
+                clear();
+                handleClose();
+                getAllCategories();
+                toast.success('Salary category saved successfully');
         }).catch((e)=>{
-            console.log(e);
+            handleClose()
+            toast.error('Not saved , please contact DEV');
         });
     }
+
+    const clear=()=>{
+        setName("");
+        setRate(0);
+        setNote("");
+    }
+
+    const getAllCategories=()=>{
+        axios.get(url+"/v1/salary-category/get-all-categories")
+            .then((res)=>{
+                setCategoryList(res.data.categoryList);
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+    }
+
+    const handleUpdate=()=>{
+        handleOpen();
+        const data={
+            "categoryName":name,
+            "perHourRate": rate,
+            "note": note
+        };
+        axios.put(url+`/v1/salary-category/update-salary-category/${categoryId}`,data)
+            .then((res)=>{
+                handleCloseDialog();
+                getAllCategories();
+                handleClose();
+                clear();
+                toast.success('Salary category updated!');
+            })
+            .catch((e)=>{
+                console.log(e)
+                handleClose();
+                toast.error('Not updated , please contact DEV');
+            })
+    }
+
+    useEffect(()=>{
+        getAllCategories();
+    },[])
     return (
         <div>
-            <h1 className='text-2xl font-bold'>Registration</h1>
+            <Toaster richColors position="top-center" />
+            <Backdrop
+                sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                open={open}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <h1 className='text-2xl font-bold'>Category Registration</h1>
             <br/>
             <form onSubmit={handleSubmit}>
 
                 <div className='my-2'>
                     <label>Category Name</label>
-                    <input onChange={(e)=>setName(e.target.value)} className='ml-[55px] bg-amber-50' type='text'/>
+                    <input value={name} onChange={(e)=>setName(e.target.value)} required className='ml-[55px] bg-amber-50' type='text'/>
                 </div>
                 <div className='my-2'>
                     <label>Per Hour Rate</label>
-                    <input onChange={(e)=>setRate(e.target.value)} className='ml-[65px] bg-amber-50' type='text' required/>
+                    <input value={rate} onChange={(e)=>setRate(e.target.value)} className='ml-[65px] bg-amber-50' type='Number' min='1' step='0.01' required/>
                 </div>
                 <div className='my-2'>
                     <label>Note</label>
-                    <input onChange={(e)=>setNote(e.target.value)} className='ml-[135px] bg-amber-50' type='text'/>
+                    <input value={note} onChange={(e)=>setNote(e.target.value)} className='ml-[135px] bg-amber-50' type='text'/>
                 </div>
                 <div className='flex items-center justify-between'>
                     <Button variant="contained" color="success" type="submit">
                         Save
                     </Button>
+                </div>
+
+                <hr className='my-5'/>
+                <div>
+                    <TableContainer component={Paper} sx={{ overflowX: "auto" }}>
+                        <Table  size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Category Id</TableCell>
+                                    <TableCell align="center">Name</TableCell>
+                                    <TableCell align="center">Rate (rs/h)</TableCell>
+                                    <TableCell align="center">note</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {categoryList.map((category) => (
+                                    <TableRow
+                                        key={category._id}
+                                    >
+
+                                        <TableCell align="center">{category.categoryId}</TableCell>
+                                        <TableCell align="center">{category.categoryName}</TableCell>
+                                        <TableCell align="center">{category.perHourRate}</TableCell>
+                                        <TableCell align="center">{category.note}</TableCell>
+                                        <TableCell align="center"><CreateIcon onClick={()=>handleClickOpen(category)}  style={{ cursor: "pointer" }}/></TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+
+                <div>
+                    <Dialog open={openDialog} onClose={handleClose}>
+                        <DialogTitle>Update Category</DialogTitle>
+                        <DialogContent>
+                            <TextField
+                                margin="dense"
+                                label="Category Name"
+                                name="categoryName"
+                                fullWidth
+                                value={name}
+                                onChange={(e)=>setName(e.target.value)}
+                            />
+                            <TextField
+                                margin="dense"
+                                label="Per Hour Rate"
+                                name="perHourRate"
+                                type="number"
+                                fullWidth
+                                value={rate}
+                                onChange={(e)=>setRate(e.target.value)}
+                            />
+                            <TextField
+                                margin="dense"
+                                label="Note"
+                                name="note"
+                                fullWidth
+                                value={note}
+                                onChange={(e)=>setNote(e.target.value)}
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleCloseDialog}>Cancel</Button>
+                            <Button variant="contained" onClick={handleUpdate}>
+                                Update
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
 
             </form>
